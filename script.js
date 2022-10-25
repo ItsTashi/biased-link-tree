8dfb31ee1c9cf644ee9056f6d8bfb9be43b90533
Tashinga Chikwanha <47496233+ItsTashi@users.noreply.github.com>
2022-10-25 10:00:00 +0200

    cleaing up

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
-        let icon = document.getElementById("toggle");
-        icon.onclick = function(){
-            let image = document.getElementById("avatar");
-            let currentImage = image.getAttribute("src").substring(0, 1);
-            if (!Number(currentImage)) {
-                currentImage++;
-                image.setAttribute("src", `${currentImage}.jpg`);
-            } else {
-                currentImage--;
-                image.setAttribute("src", `${currentImage}.jpg`);
-            }
-            document.body.classList.toggle("dark");
-            icon.classList.toggle("fa-sun-o");
+document.getElementById("toggle").onclick = function(){
+    let image = document.getElementById("avatar");
+        let currentImage = image.getAttribute("src").substring(0, 1);
+        if (!Number(currentImage)) {
+            currentImage++;
+            image.setAttribute("src", `${currentImage}.jpg`);
+        } else {
+            currentImage--;
+            image.setAttribute("src", `${currentImage}.jpg`);
         }
-
+        document.body.classList.toggle("dark");
+        document.getElementById("toggle").classList.toggle("fa-sun-o");
+        }
